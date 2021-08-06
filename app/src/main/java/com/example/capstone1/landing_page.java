192c2e74309e8d2e05015783876cfdb1e829044d
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-08-06 16:19:38 +0800

    second commit

--- app/src/main/java/com/example/capstone1/landing_page.java
+++ app/src/main/java/com/example/capstone1/landing_page.java
@@ -2,7 +2,10 @@ package com.example.capstone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class landing_page extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class landing_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        ImageView button = (ImageView) findViewById(R.id.nextbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
     }
 }
