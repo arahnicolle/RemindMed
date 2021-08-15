a13541ec9eda9af8e38e3e2f47f93413a3f7b45c
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-08-15 21:14:17 +0800

    update

--- app/src/main/java/com/example/capstone1/main_page.java
+++ app/src/main/java/com/example/capstone1/main_page.java
@@ -7,29 +7,67 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class main_page extends AppCompatActivity {
 
-    private Button createAccount;
+    private Button create_Account;
     private Button login;
     private Button guest;
+    FirebaseAuth rootAuthen;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        createAccount = (Button) findViewById(R.id.Create_btn);
+
+        create_Account = (Button) findViewById(R.id.Create_btn);
         login = (Button) findViewById(R.id.login_button);
         guest = (Button) findViewById(R.id.guest_btn);
-        createAccount.setOnClickListener(new View.OnClickListener() {
+        rootAuthen = FirebaseAuth.getInstance();
+        currentUser = rootAuthen.getCurrentUser();
+
+        //guest
+/*
+        rootAuthen.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = rootAuthen.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(main_page.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+ */
+        guest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                opencreate_account();
+                openInstructione_slideone();
             }
         });
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openlogin_page();
+            }
+        });
+
+        create_Account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opencreate_account();
             }
         });
     }
@@ -44,4 +82,13 @@ public class main_page extends AppCompatActivity {
         startActivity(intent);
     }
     //guest
+    public void openInstructione_slideone(){
+        Intent intent = new Intent(this, Instruction_slideone.class);
+        startActivity(intent);
+    }
+
+    //public void Main_To_Create (View view){
+      //  Intent intent = new Intent(main_page.this, create_account.class);
+        //startActivity(intent);
+    //}
 }
