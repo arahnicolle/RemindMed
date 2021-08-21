fdddfd67a48339c650f0f4d4ba09575132bdabf3
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-08-21 19:06:51 +0800

    added new

---
+++ app/src/main/java/com/example/capstone1/new_measurements_hours_of_sleep.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_measurements_hours_of_sleep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_measurements_hours_of_sleep);
+    }
+}
